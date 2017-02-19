9d97e370424e192e85bc4995a006186a075089e5
Alice Easter <alice.easter@mymail.champlain.edu>
2017-02-19 10:16:12 -0500

    More letters

--- model/morse_word.js
+++ model/morse_word.js
@@ -24,13 +24,35 @@ MorseWord.prototype.morseForLetter = function(letter) {
     } else if (letter.toLowerCase() == "g") {
         return ["-", "-", "."];
     } else if (letter.toLowerCase() == "h") {
-        return (".", ".", ".", ".");
+        return [".", ".", ".", "."];
     } else if (letter.toLowerCase() == "i") {
         return [".", "."];
     } else if (letter.toLowerCase() == "j") {
         return [".", "-", "-", "-"];
     } else if (letter.toLowerCase() == "k") {
         return ["-", ".", "-"];
+    } else if (letter.toLowerCase() == "l") {
+        return [".", "-", ".", "."];
+    } else if (letter.toLowerCase() == "m") {
+        return ["-", "-"];
+    } else if (letter.toLowerCase() == "n") {
+        return ["-", "."];
+    } else if (letter.toLowerCase() == "o") {
+        return ["-", "-", "-"];
+    } else if (letter.toLowerCase() == "p") {
+        return [".", "-", "-", "."];
+    } else if (letter.toLowerCase() == "q") {
+        return ["-", "-", ".", "-"];
+    } else if (letter.toLowerCase() == "r") {
+        return [".", "-", "."];
+    } else if (letter.toLowerCase() == "s") {
+        return [".", ".", "."];
+    } else if (letter.toLowerCase() == "t") {
+        return ["-"];
+    } else if (letter.toLowerCase() == "u") {
+        return [".", ".", "-"];
+    } else if (letter.toLowerCase() == "v") {
+        return [".", ".", ".", "-"];
     }
 };
 
