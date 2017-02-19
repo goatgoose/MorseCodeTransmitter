bd1eaa6b88565b2df920f8872df701aa6762b7e9
Alice Easter <alice.easter@mymail.champlain.edu>
2017-02-19 00:30:48 -0500

    Added letters to translator.

--- model/morse_word.js
+++ model/morse_word.js
@@ -13,6 +13,24 @@ MorseWord.prototype.morseForLetter = function(letter) {
         return [".", "-"];
     } else if (letter.toLowerCase() == "b") {
         return ["-", ".", ".", "."];
+    } else if (letter.toLowerCase() == "c") {
+        return ["-", ".", "-", "."];
+    } else if (letter.toLowerCase() == "d") {
+        return ["-", ".", "."];
+    } else if (letter.toLowerCase() == "e") {
+        return ["."];
+    } else if (letter.toLowerCase() == "f") {
+        return [".", ".", "-", "."];
+    } else if (letter.toLowerCase() == "g") {
+        return ["-", "-", "."];
+    } else if (letter.toLowerCase() == "h") {
+        return (".", ".", ".", ".");
+    } else if (letter.toLowerCase() == "i") {
+        return [".", "."];
+    } else if (letter.toLowerCase() == "j") {
+        return [".", "-", "-", "-"];
+    } else if (letter.toLowerCase() == "k") {
+        return ["-", ".", "-"];
     }
 };
 
